10d32de01aeef108d708d28feb0dfde66431f5ad
Mark Kulik <kulik_mark@mail.ru>
2022-01-31 23:13:09 +0300

    Initial Commit

---
+++ CardsFitness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardsFitness
+//
+//  Created by Марк Кулик on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
