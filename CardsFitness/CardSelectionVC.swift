57f5a63a70e5c476182d47fdb8f4e6def55cfa6f
Mark Kulik <kulik_mark@mail.ru>
2022-02-03 23:39:21 +0300

    Changes added. Rules second screen was created.

---
+++ CardsFitness/CardSelectionVC.swift
@@ -0,0 +1,33 @@
+//
+//  CardSelectionVC.swift
+//  CardsFitness
+//
+//  Created by Марк Кулик on 31.01.2022.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    @IBOutlet var cardImageView: UIImageView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+    }
+    
+}
